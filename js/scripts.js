5c9ea04dc74bb89c7b56a7a8c105ac8e4dd729a1
RuslanSalimov <mail-spam2012@yandex.ru>
2021-04-26 05:27:18 +0500

    js done

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
     /**
+     * Вычисляет величину скролла на странице
+     * @returns {number}
+     */
+    const calcScroll = () => {
+        let div = document.createElement('div');
+        div.style.width = '500px';
+        div.style.height = '500px';
+        div.style.overflowY = 'scroll';
+        div.style.visibility = 'hidden';
+        document.body.appendChild(div);
+        let scrollWidth = div.offsetWidth - div.clientWidth;
+        div.remove();
+        return scrollWidth;
+    }
+    const toggleLockBody = () => {
+        const body = document.body;
+        body.classList.toggle('lock');
+        const bodyScroll = calcScroll();
+        if(body.classList.contains('lock')){
+            body.style.marginRight = `${bodyScroll}px`;
+        } else {
+            body.style.marginRight = `0`;
+        }
+    }
+    /**
      * Функция плавного скролла до элемента, чистая, работает как вверх, так и вниз (писал сам)))
      * @param element - ссылка на элемент
      * @param duration - продолжительность скролла в мс
@@ -51,6 +76,17 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     };
 
+    const openModal = (modal) => {
+      modal.classList.remove('unblock');
+      toggleLockBody();
+        // document.body.style.overflow = 'hidden';
+    };
+    const closeModal = (modal) => {
+        modal.classList.add('unblock');
+        toggleLockBody();
+        // document.body.style.overflow = 'auto';
+    };
+
     const bindModalForm = () => {
         // forms
         const orderForms = document.querySelectorAll('.order-form');
@@ -73,6 +109,10 @@ document.addEventListener('DOMContentLoaded', () => {
                         return response.text()
                     }).then((response) => {
                         targetForm.reset();
+                        const modal = document.querySelector('.modal');
+                        if(!modal.classList.contains('unblock')){
+                            closeModal(modal);
+                        }
                     }).catch((error) => {
                         console.error(error);
                     });
@@ -86,7 +126,15 @@ document.addEventListener('DOMContentLoaded', () => {
         const button = document.querySelector('.main-display__button');
         const modal = document.querySelector('.modal__overlay');
         button.addEventListener('click', (e) => {
-            modal.classList.toggle('unblock');
+            e.preventDefault();
+            openModal(modal);
+        });
+        modal.addEventListener('click', (e) => {
+            let target = e.target;
+            const isModal = target.closest('.modal-content');
+            if(!isModal){
+                closeModal(modal);
+            }
         })
     };
     bindModal();
